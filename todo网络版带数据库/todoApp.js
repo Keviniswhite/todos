8e2f2b96d104472d6eea9090113aa7ffd68dba3a
秦凯 <937817514@qq.com>
2020-05-22 00:24:30 +0800

    写到router.js 引入了Express

--- todo网络版带数据库/todoApp.js
+++ todo网络版带数据库/todoApp.js
@@ -1,14 +1,30 @@
 const express = require('express')
 
 const app = express()
+const bodyParser = require('body-parser')
+const router = require('./router/init.js')
+app.use(express.static("public"))
+
+app.use(bodyParser.json())
 
 
-app.get()
 
-const port = 3000
+
 
 const log = console.log.bind(console)
+
+
+
+
+
+
+
+const port = 3000
 const todoInit = () => {
     log(` run in ${port}`)
 }
+
+
 app.listen(port, todoInit)
+
+module.exports = app
