53b912fe4193ac28d549133c295f8e8b9d371c4b
秦凯 <937817514@qq.com>
2020-05-22 00:28:46 +0800

    装了 mysql 的依赖

--- todo网络版带数据库/todoApp.js
+++ todo网络版带数据库/todoApp.js
@@ -3,8 +3,14 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const router = require('./router/init.js')
-app.use(express.static("public"))
 
+// 设置静态资源目录
+app.use(express.static("public"))
+    // 设置html 模板引擎是 pug
+app.set('view engine', 'pug')
+
+
+// 解析交互数据
 app.use(bodyParser.json())
 
 
